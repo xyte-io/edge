8a22d6d2a84f929745cd73df3158eee816f232d3
Boris Dinkevich <boris@xyte.io>
2025-04-08 18:05:12 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# Xyte Connect+ Edge
+
+## Download the installation script and run it manually
+1. Download the runner script.
+
+```
+wget https://raw.githubusercontent.com/xyte-io/edge/refs/heads/main/edge_runner_script.sh
+```
+
+2. Enable script execution
+
+```
+chmod +x ./edge_runner_script.sh
+```
+
+3. Run the script
+
+```
+./edge_runner_script.sh
+```
+
+## One line installation
+
+```
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/xyte-io/edge/refs/heads/main/edge_runner_script.sh)"
+```
---
+++ edge_runner_script.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+# Check if telnet is installed
+if ! command -v telnet &>/dev/null; then
+    echo "Error: telnet is not installed. Please install it and try again."
+    exit 1
+fi
+
+# Verify Connection to the production server before trying to run docker
+SERVER="eu1.edge.xyte.io"
+PORT=443
+
+if echo "quit" | telnet "$SERVER" "$PORT" 2>/dev/null | grep -q "Connected"; then
+    echo "Connection successful"
+else
+    echo "Failed to connect to $SERVER on port $PORT."
+    exit 1
+fi
+
+# Start the Docker container (suppress stdout)
+docker run -d --privileged --network host --pull always --restart always -v $(pwd)/edge_data:/xyte/edge_data --name xyte_edge xytetech/xyte_edge > /dev/null
+
+# proxy name file (created by container)
+file="$(pwd)/edge_data/proxy_name.txt"
+
+# Wait for the file to exist (for the first run it might not exist because it is created by the containers first run)
+while [ ! -f "$file" ]; do
+  sleep 1  # Sleep for 1 second before checking again
+done
+
+# Once the file exists, Read the content of the file into a variable
+file_content=$(cat "$file")
+
+# Print proxy name
+echo "Proxy Name: $file_content"
